7ad0bc093199dd7caca845f086bfefd5a6f01bd2
victorpreston <prestonvictor25@gmail.com>
2024-02-08 09:12:53 +0300

    30-Days-Of-JavaScript

---
+++ day_03/Exercise_1.js
@@ -0,0 +1,14 @@
+/*
+You must define the following variables:
+    A number called myNumber which contains the number 4;
+    A string called myString which contains the sentence Variables are great.;
+    A boolean called myBoolean which contains the value false;
+*/
+
+const myNumber = 4;
+const myString = "Variables are great";
+const myBoolean = false;
+
+console.log("myNumber is " + myNumber);
+console.log("myString is " + myString);
+console.log("myBoolean is " + myBoolean);
