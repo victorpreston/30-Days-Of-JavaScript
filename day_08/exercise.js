1ad73297ca926ec20b36d571d7266fb3e248a712
victorpreston <prestonvictor25@gmail.com>
2024-03-07 22:41:16 +0300

    day_08

---
+++ day_08/exercise.js
@@ -0,0 +1,19 @@
+/**
+ * You must define an object called person with the following members:
+The member firstName of the person is "Jack"
+The member lastName of the person is "Smith"
+The member age of the person is 19
+The member employed of the person is true
+ */
+
+var person ={
+    firstName: "Jack",
+    lastName: "Smith",
+    age: 19,
+    employed: true
+};
+
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.age);
+console.log(person.employed);
