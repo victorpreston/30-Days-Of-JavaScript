8916a447a9fe6878f82179f97674e2e144a1048c
victorpreston <prestonvictor25@gmail.com>
2024-03-10 16:10:25 +0300

    day_9

--- day_09/event-loops.js
+++ day_09/event-loops.js
@@ -26,3 +26,23 @@ new Promise(function (resolve, reject) {
     console.log(res);
   }, 0);
 });
+
+setTimeout(() => {
+  console.log("setTimeout 1");
+  Promise.resolve().then(() => {
+    console.log("promise 1");
+  });
+}, 0);
+
+new Promise(function (resolve, reject) {
+  console.log("promise 2");
+  setTimeout(function () {
+    console.log("setTimeout 2");
+    resolve("resolve 1");
+  }, 0);
+}).then((res) => {
+  console.log("dot then 1");
+  setTimeout(() => {
+    console.log(res);
+  }, 0);
+});
